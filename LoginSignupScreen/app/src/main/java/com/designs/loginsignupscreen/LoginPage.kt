be77aae86afdd4ca2d8e05d7c376174c3bd181b9
Vaibhavbansal15 <vaibhavbansal1501@gmail.com>
2024-01-23 23:56:00 +0530

    created forgot password page

--- LoginSignupScreen/app/src/main/java/com/designs/loginsignupscreen/LoginPage.kt
+++ LoginSignupScreen/app/src/main/java/com/designs/loginsignupscreen/LoginPage.kt
@@ -10,9 +10,16 @@ class LoginPage : AppCompatActivity() {
         setContentView(R.layout.activity_login_page)
 
         val signup_btn = findViewById<TextView>(R.id.login_footer_signup)
+        val resetPasswordBtn : TextView = findViewById(R.id.resetPassword)
+
         signup_btn.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+        resetPasswordBtn.setOnClickListener{
+            startActivity(Intent(this, ForgotPassword::class.java))
+        }
+
     }
 }
